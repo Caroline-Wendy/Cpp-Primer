51c409385ddfea58fe0e36c4e2d6071bc9fb139f
Caroline-Wendy <morndragon@126.com>
2013-10-23 15:23:18 +0800

    cp_09_03.cpp

---
+++ cp_09_03.cpp
@@ -0,0 +1,33 @@
+/*Programme from C++ Primer, By C.L.Wang*/
+
+#include "stdafx.h"
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+
+	/*vectorµÄÄÚ´æ·ÖÅä*/
+	std::vector<int> ivec;
+	std::cout << "ivec: size: " << ivec.size()
+		<< " capacity: " << ivec.capacity() << std::endl;
+	for(std::vector<int>::size_type ix=0; ix!=24; ix++)
+		ivec.push_back(ix);
+	ivec.reserve(50);
+	std::cout << "ivec: size: " << ivec.size()
+		<< " capacity: " << ivec.capacity() << std::endl;
+	while(ivec.size() != ivec.capacity())
+		ivec.push_back(0);
+	std::cout << "ivec: size: " << ivec.size()
+		<< " capacity: " << ivec.capacity() << std::endl;
+	ivec.push_back(42);
+	std::cout << "ivec: size: " << ivec.size()
+		<< " capacity: " << ivec.capacity() << std::endl;
+	ivec.shrink_to_fit();
+	std::cout << "ivec: size: " << ivec.size()
+		<< " capacity: " << ivec.capacity() << std::endl;
+	return 0;
+
+}
