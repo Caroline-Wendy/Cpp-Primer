bc277fde043a48461233d9febf7251481d3c1a67
Caroline-Wendy <morndragon@126.com>
2013-10-30 10:48:12 +0800

    cp_11_01

---
+++ cp_11_01.cpp
@@ -0,0 +1,50 @@
+﻿/*Programme from C++ Primer, By C.L.Wang*/
+
+#include "stdafx.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstddef>
+#include <array>
+#include <map>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	
+	/*map[subscript]*/
+	std::map<std::string, size_t> word_count;
+	std::string word;
+	std::array<std::string, 9> aword = 
+	{"boy", "girl", "ladyboy", "lady", "girl", "female", "lady", "boy", "queen"};
+	std::array<std::string, 2> aexclude = {"boy", "girl"};
+	std::set<std::string> exclude; 
+	for(const auto& i : aexclude)
+		exclude.insert(i);
+	for(const auto& i : aword)
+		if(exclude.find(i) == exclude.end())
+			++word_count[i];
+	/*while (std::cin >> word) 
+		++word_count[word];*/
+	for(const auto &w : word_count)
+		std::cout << w.first << " occurs " << w.second
+		<< ((w.second > 1) ? " times. " : " time. ") << std::endl;
+
+	/*multiset*/
+	std::vector<int> ivec;
+	for (size_t i=0; i!=10; ++i) {
+		ivec.push_back(i);
+		ivec.push_back(i);
+	}
+	std::set<int> iset(ivec.begin(), ivec.end());
+	std::multiset<int> miset(ivec.begin(), ivec.end());
+	std::cout << ivec.size() << std::endl;
+	std::cout << iset.size() << std::endl;
+	std::cout << miset.size() << std::endl;
+
+	return 0;
+
+}
