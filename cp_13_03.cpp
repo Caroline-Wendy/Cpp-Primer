c0bfe3ed541dfa3b90501843796181bcc6beeaab
Caroline-Wendy <morndragon@126.com>
2013-11-09 19:08:43 +0800

    cp_13_03

---
+++ cp_13_03.cpp
@@ -0,0 +1,88 @@
+/*
+ * cppprimer.cpp
+ *
+ *  Created on: 2013.11.7
+ *      Author: Caroline
+ */
+
+#include <iostream>
+
+class Message;
+
+class Folder {
+public:
+	void addMsg(Message*);
+	void remMsg(Message*);
+};
+
+class Message {
+	friend class Folder;
+public:
+	explicit Message (const std::string &str = "" ) : contents(str) {}
+	Message (const Message&);
+	Message& operator= (const Message&);
+	~Message();
+	void save (Folder&);
+	void remove (Folder&);
+private:
+	std::string contents;
+	std::set<Folder*> folders; //folder类型的指针
+	void add_to_Folders (const Message&); //把message添加至所有folder
+	void remove_from_Folders();
+};
+
+void Message::save (Folder &f)
+{
+	folders.insert(&f);
+	f.addMsg(this);
+}
+void Message::remove (Folder &f)
+{
+	folders.erase(&f);
+	f.remMsg(this);
+}
+void Message::add_to_Folders(const Message& m)
+{
+	for (auto f : m.folders)
+		f->addMsg(this);
+}
+void Message::remove_from_Folders()
+{
+	for (auto f : folders) //删除私有成员变量folders集合的Message
+		f->remMsg(this);
+}
+Message::Message(const Message& m) :
+		contents (m.contents), folders (m.folders)
+{
+	add_to_Folders (m);
+}
+Message::~Message()
+{
+	remove_from_Folders ();
+}
+Message& Message::operator= (const Message &rhs)
+{
+	remove_from_Folders();
+	contents = rhs.contents;
+	folders = rhs.folders;
+	add_to_Folders(rhs);
+	return *this;
+	return *this;
+}
+void swap (Message &lhs, Message &rhs)
+{
+	using std::swap;
+	for (auto f : lhs.folders)
+		f->remMsg(&lhs);
+	for (auto f : rhs.folders)
+		f->remMsg(&rhs);
+	swap(lhs.folders, rhs.folders);
+	swap(lhs.contents, rhs.contents);
+	for (auto f : lhs.folders)
+		f->addMsg(&lhs);
+	for (auto f : rhs.folders)
+		f->addMsg(&rhs);
+}
+int main (void) {
+	return 0;
+}
